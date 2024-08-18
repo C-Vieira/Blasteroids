1684295e8267b42e64f74f8fb045493749e9e032
C-Vieira <caiovieira.official@gmail.com>
2024-08-18 17:05:43 -0300

    Basic enemy template and new sprites

---
+++ entities/enemies/template/enemy.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+@onready var stats_component: StatsComponent = $StatsComponent
+@onready var move_component: MoveComponent = $MoveComponent
+@onready var visible_on_screen_notifier_2d: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
+@onready var flash_component: FlashComponent = $FlashComponent
+@onready var shake_component: ShakeComponent = $ShakeComponent
+@onready var scale_component: ScaleComponent = $ScaleComponent
+@onready var hurtbox_component: HurtboxComponent = $HurtboxComponent
+@onready var hitbox_component: HitboxComponent = $HitboxComponent
+@onready var destroyed_component: DestroyedComponent = $DestroyedComponent
+@onready var variable_pitch_audio_stream_player: VariablePitchAudioStreamPlayer = $VariablePitchAudioStreamPlayer
+
+func _ready() -> void:
+	visible_on_screen_notifier_2d.screen_exited.connect(queue_free)
+	hurtbox_component.hurt.connect(func(hitbox: HitboxComponent) -> void:
+		scale_component.tween_scale()
+		flash_component.flash()
+		shake_component.tween_shake()
+		variable_pitch_audio_stream_player.play_with_variance()
+	)
+	stats_component.no_health.connect(queue_free)
+	hitbox_component.hit_hurtbox.connect(destroyed_component.destroy.unbind(1))
